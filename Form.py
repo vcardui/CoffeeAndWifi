498dd811fe3063d40e93d1f2bc29208e852fa17a
vanessa <vanessa@reteguin.com>
2025-05-03 21:19:10 -0600

    Initial commit

---
+++ Form.py
@@ -0,0 +1,60 @@
+# +----------------------------------------------------------------------------+
+# | CARDUI WORKS v1.0.0
+# +----------------------------------------------------------------------------+
+# | Copyright (c) 2024 - 2025, CARDUI.COM (www.cardui.com)
+# | Vanessa Reteguín <vanessa@reteguin.com>
+# | Released under the MIT license
+# | www.cardui.com/carduiframework/license/license.txt
+# +----------------------------------------------------------------------------+
+# | Author.......: Vanessa Reteguín <vanessa@reteguin.com>
+# | First release: May 2nd, 2025
+# | Last update..: May 3rd, 2025
+# | WhatIs.......: Form - Class
+# +----------------------------------------------------------------------------+
+
+# ------------ Resources / Documentation involved -------------
+# Flask-WTF Basic fields: https://wtforms.readthedocs.io/en/3.0.x/fields/#basic-fields
+# Flask-WTF Validators: https://wtforms.readthedocs.io/en/3.0.x/validators/#module-wtforms.validators
+# Dynamic Forms with Flask: https://blog.miguelgrinberg.com/post/dynamic-forms-with-flask
+
+# Flaskform class pass arguments for constructor:
+# Struggling to understand FlaskForm with concurrent use of a constructor with super() function: https://www.reddit.com/r/flask/comments/pag4yl/struggling_to_understand_flaskform_with/
+# (It inheritances)
+# What Does Super().__Init__(*Args, **Kwargs) Do in Python?: https://www.geeksforgeeks.org/what-does-super-__init__args-kwargs-do-in-python/
+
+# ------------------------- Libraries -------------------------
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField, TimeField, SelectField
+from wtforms.validators import InputRequired, Regexp
+
+import csv
+
+# ------------------------- Classes -------------------------
+class CafeForm(FlaskForm):
+    name = StringField('Name', [
+        InputRequired("Enter cafe's name"),
+        Regexp(
+            regex="^[A-Za-z0-9 áéíóúüñÁÉÍÓÚÜÑàâäæçèéêëîïôœùûüÿÀÂÆÇÈÉÊËÎÏÔŒÙÛÜŸß .&'@#-]+$",
+            message="Remove any not alphabetical characters or emoji"
+        )
+    ])
+
+    url = URLField('Cafe Location on google maps', [
+        InputRequired("Add location")
+    ])
+
+    openingTime = TimeField('Opening time', format='%H:%M')
+
+    closingTime = TimeField('Closing time', format='%H:%M')
+
+    coffeeRating = SelectField('Coffee Rating', coerce=str)
+    wifiRating = SelectField('Wifi Rating', coerce=str)
+    powerRating = SelectField('Power Rating', coerce=str)
+
+    submit = SubmitField(label="Add")
+
+    def __init__(self, ratings: dict = None, *args, **kwargs):
+        super(CafeForm, self).__init__(*args, **kwargs)
+        self.coffeeRating.choices = ratings['Coffe']
+        self.wifiRating.choices = ratings['Wifi']
+        self.powerRating.choices = ratings['Power']
---
+++ main.py
@@ -0,0 +1,100 @@
+# +----------------------------------------------------------------------------+
+# | CARDUI WORKS v1.0.0
+# +----------------------------------------------------------------------------+
+# | Copyright (c) 2024 - 2025, CARDUI.COM (www.cardui.com)
+# | Vanessa Reteguín <vanessa@reteguin.com>
+# | Released under the MIT license
+# | www.cardui.com/carduiframework/license/license.txt
+# +----------------------------------------------------------------------------+
+# | Author.......: Vanessa Reteguín <vanessa@reteguin.com>
+# | First release: May 2nd, 2025
+# | Last update..: May 3rd, 2025
+# | WhatIs.......: CoffeAndWifi - Main
+# +----------------------------------------------------------------------------+
+
+# ------------ Resources / Documentation involved -------------
+# Jinja Expressions: https://jinja.palletsprojects.com/en/stable/templates/#jinja-filters.length
+
+
+# ------------------------- Libraries -------------------------
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+import csv
+from datetime import datetime
+
+from Form import CafeForm
+
+# ------------------------- Variables -------------------------
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# Cafe's Data
+with open('cafe-data.csv', newline='', encoding='utf-8') as cafe_csv:
+    cafe_data = csv.reader(cafe_csv, delimiter=',')
+    cafe_list = []
+    for row in cafe_data:
+        cafe_list.append(row)
+
+# Cafe's Database rating options
+ratings = {
+  "Coffe": [],
+  "Wifi": [],
+  "Power": []
+}
+
+icons = {"Coffe": "☕️", "Wifi": "💪", "Power": "🔌"}
+
+for category in ratings:
+    rate = ""
+    for i in range(0, 5):
+        if i == 0 and category != "Coffe":
+            ratings[category].append((i, "✘"))
+        rate += icons[category]
+        ratings[category].append((i+1, rate))
+
+# print(ratings)
+# {'Coffe': [(1, '☕️'), (2, '☕️☕️'), (3, '☕️☕️☕️'), (4, '☕️☕️☕️☕️'), (5, '☕️☕️☕️☕️☕️')], 'Wifi': [(0, '✘'), (1, '💪'), (2, '💪💪'), (3, '💪💪💪'), (4, '💪💪💪💪'), (5, '💪💪💪💪💪')], 'Power': [(0, '✘'), (1, '🔌'), (2, '🔌🔌'), (3, '🔌🔌🔌'), (4, '🔌🔌🔌🔌'), (5, '🔌🔌🔌🔌🔌')]}
+
+# ----------------------- Flask routes ------------------------
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add_cafe():
+    form = CafeForm(ratings)
+    if form.validate_on_submit():
+
+        newCafe = []
+        for i in form.data:
+            newCafe.append(form.data[i])
+
+        newCafe = newCafe[:-2]
+
+        newCafe[2] = newCafe[2].strftime("%H.%M %p")
+        newCafe[3] = newCafe[3].strftime("%H.%M %p")
+
+        newCafe[4] = ratings["Coffe"][int(newCafe[4]) - 1][1]
+        newCafe[5] = ratings["Wifi"][int(newCafe[5]) - 1][1]
+        newCafe[6] = ratings["Power"][int(newCafe[6]) - 1][1]
+
+        # print(newCafe)
+
+        with open('cafe-data.csv', mode='a', newline='') as cafe_csv:
+            write = csv.writer(cafe_csv)
+            write.writerow(newCafe)
+
+        # print("newCaffe added")
+        return render_template('cafes.html', cafes=cafe_list)
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    return render_template('cafes.html', cafes=cafe_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
